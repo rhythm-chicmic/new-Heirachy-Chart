a406df16e38edcdb442d4082510a59f3dd5ec9e3
Rhythm08 <rhythm.sharma@chicmic.co.in>
2023-05-01 17:49:07 +0530

    new Heirarchy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:ExampleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BasicPrimitivesModule } from 'ngx-basic-primitives';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleComponent } from './example/example.component';
+import { FormsModule } from '@angular/forms';
+import {MatSliderModule} from '@angular/material/slider'; 
+import {MatCardModule} from '@angular/material/card'; 
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatRadioModule,
+    MatCardModule,
+    FormsModule,
+    MatSliderModule,
+    BasicPrimitivesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
